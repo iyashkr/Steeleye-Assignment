c23b11a1befe04423350518adceef7ff34443c0d
iyashkr <kumaryash1212@gmail.com>
2023-04-23 22:25:49 +0530

    release version

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import List from "./list";
 
 function App() {
+  const itemsList = [
+    {
+      text: "one",
+    },
+    {
+      text: "two",
+    },
+    {
+      text: "three",
+    },
+
+    {
+      text: "four",
+    },
+    {
+      text: "five",
+    },
+    {
+      text: "six",
+    },
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <List items={itemsList} />
     </div>
   );
 }
